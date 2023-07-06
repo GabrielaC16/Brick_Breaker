aa9a95bb5f7ff7823a2423a399817160753a62e7
Gabriela Colque <gabriela.colque.u@uni.pe>
2023-07-06 02:15:37 -0500

    update inicio del juego

--- combi.py
+++ combi.py
@@ -27,7 +27,7 @@ def main():
 
     # caracteristicas del rectángulo
     color_rectangle = (1, 1, 1)     # color blanco
-    rectangle = Rectangle(400, 0, 100, 20, color_rectangle)
+    rectangle = Rectangle(350, 0, 100, 20, color_rectangle)
 
     # lista de posiciones para los ladrillos
     all_position = fractales.gen_positions()
@@ -61,64 +61,87 @@ def main():
 
     # Características de la pelota
     ball = Ball(400, 60, 15)
-    ball.speed_x = 0.6
-    ball.speed_y = 0.6
+    ball.speed_x = 0
+    ball.speed_y = 0
+
+    game_started = False
 
     # Ciclo principal del juego
     while True:
         i = -1
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not game_started:
+                    game_started = True
+                    ball.speed_x = 0.6
+                    ball.speed_y = 0.6
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
 
         glClear(GL_COLOR_BUFFER_BIT)
 
+        
         # Movimiento del rectángulo
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_LEFT] and rectangle.x >0:
-            rectangle.x -= 2
-
-        if keys[pygame.K_RIGHT] and rectangle.x < 700:
-            rectangle.x += 2
-
         # Movimiento de la bola
         ball.move(WINDOW_HEIGHT, WINDOW_WIDTH)
+        
 
-        # Colisiones entre la bola y el rectángulo
-        if ball.collides_with_rectangle(rectangle):
-            #print(len(squares))
-            ball.speed_y *= -1
+        if not game_started:
 
-        # Colisiones entre la bola y los ladrillos nivel 1
-        for square in squares_nivel1:
-            if ball.collides_with_rectangle(square):
-                #print("tocaste cuadrado level 1")
-                squares_nivel1.remove(square)
+            if keys[pygame.K_LEFT] and rectangle.x >0:
+                rectangle.x -= 2
+                ball.x -= 2
+
+            if keys[pygame.K_RIGHT] and rectangle.x < 700:
+                rectangle.x += 2
+                ball.x += 2
+
+
+        if game_started:
+
+            if keys[pygame.K_LEFT] and rectangle.x >0:
+                rectangle.x -= 2
+
+            if keys[pygame.K_RIGHT] and rectangle.x < 700:
+                rectangle.x += 2
+
+            # Colisiones entre la bola y el rectángulo
+            if ball.collides_with_rectangle(rectangle):
+                #print(len(squares))
                 ball.speed_y *= -1
-                ball.speed_x *= -1
 
-        # Colisiones entre la bola y los ladrillos nivel 2
-        for square in squares_nivel2:
-            i+=1           
-            if ball.collides_with_rectangle(square):
-                square.change_color(color1)
-                golpes_cuadrados_nivel2[i] -= 1
-                ball.speed_y *= -1
-                ball.speed_x *= -1
-                #print("tocaste cuadrado nivel 2: ",i)
-                if golpes_cuadrados_nivel2[i] == 0:
-                    squares_nivel2.remove(square)
-                    #print("Rompiste cuadrado nivel 2: ",i)
+            # Colisiones entre la bola y los ladrillos nivel 1
+            for square in squares_nivel1:
+                if ball.collides_with_rectangle(square):
+                    #print("tocaste cuadrado level 1")
+                    squares_nivel1.remove(square)
+                    ball.speed_y *= -1
+                    ball.speed_x *= -1
 
-        # Si la pelota colisiona con el suelo
-        if ball.collide_with_floor():
-            derrota.show_welcome_screen()
+            # Colisiones entre la bola y los ladrillos nivel 2
+            for square in squares_nivel2:
+                i+=1           
+                if ball.collides_with_rectangle(square):
+                    square.change_color(color1)
+                    golpes_cuadrados_nivel2[i] -= 1
+                    ball.speed_y *= -1
+                    ball.speed_x *= -1
+                    #print("tocaste cuadrado nivel 2: ",i)
+                    if golpes_cuadrados_nivel2[i] == 0:
+                        squares_nivel2.remove(square)
+                        #print("Rompiste cuadrado nivel 2: ",i)
 
-        # Si los ladrillos son cero 
-        if len(squares_nivel1 + squares_nivel2 ) == 0:
-            victoria.show_welcome_screen()
+            # Si la pelota colisiona con el suelo
+            if ball.collide_with_floor():
+                derrota.show_welcome_screen()
+
+            # Si los ladrillos son cero 
+            if len(squares_nivel1 + squares_nivel2 ) == 0:
+                victoria.show_welcome_screen()
 
         # Dibujar el rectángulo
         rectangle.draw()
